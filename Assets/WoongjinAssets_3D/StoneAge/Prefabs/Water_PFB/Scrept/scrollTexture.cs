5ab04116c6dddccb7927ff1a4d1ed7cde8037adb
99narii <122865149+99narii@users.noreply.github.com>
2023-10-23 15:08:38 +0900

    웅진 에셋

---
+++ Assets/WoongjinAssets_3D/StoneAge/Prefabs/Water_PFB/Scrept/scrollTexture.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scrollTexture : MonoBehaviour
+{
+    #region field
+    public System.Action ActionCreditScrollFinished = null;
+    public float scrollX = 0.5F;
+    public float scrollY = 0.5F;
+    public bool scrollLoop = false;
+    private float elapsed = 0f;
+    bool _startScroll = false;
+    private float _limitY = 0f;
+    #endregion
+
+    #region unity mono
+    private void Start()
+    {
+        elapsed = 0;
+    }
+    void Update()
+    {
+        if (!_startScroll && !scrollLoop)
+            return;
+
+        elapsed += Time.deltaTime;
+        float OffsetX = elapsed * scrollX;
+        float OffsetY = elapsed * scrollY;
+        GetComponent<Renderer>().material.mainTextureOffset = new Vector2(OffsetX, OffsetY);
+
+        if (OffsetY < _limitY) {
+            _startScroll = false;
+            if (ActionCreditScrollFinished != null)
+                ActionCreditScrollFinished();
+        }
+    }
+    #endregion
+
+    #region method
+    public void StartScroll()
+    {
+        _startScroll = true;
+    }
+    public void StopScroll()
+    {
+        _startScroll = false;
+    }
+    public void SetScrollLimitY(float y)
+    {
+        _limitY = y;
+    }
+    #endregion
+}
