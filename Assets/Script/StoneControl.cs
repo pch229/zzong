a89d3e6880aad2534d5ce0304e7d3460ac6445c1
Chris <chl9430@gmail.com>
2023-10-26 20:54:40 +0900

    돌판에 드래그 할때마다 먼지 파티클 활성화

--- Assets/Script/StoneControl.cs
+++ Assets/Script/StoneControl.cs
@@ -6,11 +6,52 @@ using UnityEngine;
 
 public class StoneControl : MonoBehaviour
 {
+    ParticleSystem grindingParticle;
+
+    bool isClicked = false;
+
+    void Start()
+    {
+        grindingParticle = GetComponentInChildren<ParticleSystem>();
+    }
+
+    private void Update()
+    {
+        if(isClicked)
+        {
+            RaycastHit hit;
+            ParticleSystem.EmissionModule emissionModule = grindingParticle.emission;
+
+            Debug.DrawRay(gameObject.transform.position + new Vector3(0, 0, 0.25f), Vector3.forward, UnityEngine.Color.red);
+
+            if (Physics.Raycast(gameObject.transform.position + new Vector3(0, 0, 0.25f), Vector3.forward, out hit, 1000))
+            {
+                if (hit.transform.gameObject.name == "Tool_Grinding_Plate")
+                {
+                    emissionModule.enabled = true;
+                }
+            }
+            else
+            {
+                emissionModule.enabled = false;
+            }
+        }
+    }
+
+    void OnMouseDown()
+    {
+        isClicked = true;
+    }
+
+    void OnMouseUp()
+    {
+        isClicked = false;
+    }
+
     void OnMouseDrag()
     {
         float distance = Camera.main.WorldToScreenPoint(transform.position).z;
 
-        Debug.Log(Input.mousePosition);
         Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance);
         Vector3 objPos = Camera.main.ScreenToWorldPoint(mousePos);
 
