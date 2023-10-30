51ad29b92a9e1af0fcaa83e4dca7b4aebea339cf
Chris <chl9430@gmail.com>
2023-10-30 18:18:00 +0900

    뗀석기 UI 생성, 조이스틱 이동 기능 구현

---
+++ Assets/MakingStone/Script/MovingJoystick.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MovingJoystick : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    [SerializeField] RectTransform lever;
+    [SerializeField, Range(10f, 150f)] float leverRange = 30f;
+    [SerializeField] RectTransform crossHair;
+    [SerializeField] float moveSpeed = 100f;
+
+    private RectTransform rectTransform;
+    private Vector2 inputVector;
+    private bool isInput;
+
+    void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    void Update()
+    {
+        if (isInput)
+        {
+            Vector2 pos = inputVector * moveSpeed * Time.deltaTime;
+
+            crossHair.anchoredPosition += pos;
+        }
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        ControlJoystickLever(eventData);  // 추가
+        isInput = true;    // 추가
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        ControlJoystickLever(eventData);
+    }
+
+    public void ControlJoystickLever(PointerEventData eventData)
+    {
+        var inputDir = eventData.position - rectTransform.anchoredPosition;
+
+        var clampedDir = inputDir.magnitude < leverRange ? inputDir
+            : inputDir.normalized * leverRange;
+        lever.anchoredPosition = clampedDir;
+
+        inputVector = clampedDir / leverRange;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        lever.anchoredPosition = Vector2.zero;
+        isInput = false;
+    }
+}
